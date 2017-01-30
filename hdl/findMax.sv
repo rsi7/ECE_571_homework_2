e7cf63d68784d934d4230828ded456e2df0005a0
Rehan Iqbal <rsi7.unc@gmail.com>
2017-01-29 21:36:05 -0800

    Starting to finalize findMax module

--- hdl/findMax.sv
+++ hdl/findMax.sv
@@ -31,19 +31,12 @@ module findMax (
 	/* Local parameters and variables										 */
 	/*************************************************************************/
 
-	localparam	S0	=	4'b0000;
-	localparam	S1	=	4'b0001;
-	localparam	S2	=	4'b0010;
-	localparam	S3	=	4'b0100;
-	localparam	S4	=	4'b1000;
+	localparam	IDLE		=	2'b00;		// state: FSM is waiting for start signal
+	localparam	RECEIVING	=	2'b01;		// state: FSM is receing data
+	localparam	DONE		=	2'b10;		// state: FSM has finished processing data
 
-	reg		[3:0]		state,
-	reg		[3:0]		next;
-
-	/*************************************************************************/
-	/* Global Assignments													 */
-	/*************************************************************************/
-
+	logic		[1:0]		state,			// register to hold current FSM state
+	logic		[1:0]		next;			// register to hold pending FSM state
 
 	/*************************************************************************/
 	/* FSM Block 1: reset & state advancement								 */
@@ -51,10 +44,12 @@ module findMax (
 
 	always@(posedge clk or posedge reset) begin
 
+		// reset the FSM to idle state
 		if (reset) begin
-			state <= S0;
+			state <= IDLE;
 		end
 
+		// otherwise, advance the state
 		else begin
 			state <= next;
 		end
@@ -65,22 +60,38 @@ module findMax (
 	/* FSM Block 2: state transistions								 		 */
 	/*************************************************************************/
 
-	always@(state or go or jmp) begin
+	always@(posedge clk or posedge reset) begin
 
-		next 		= 4'bx;
+		// default state assignment
+		// if case statement fails
+
+		next = 4'bx;
 
 		case (state)
+
+			// check if start was asserted
+			// if so, FSM is receiving data
+			// otherwise, keep idle
+
 			IDLE : begin
-				if (sdfsd) begin
-					sdfsd
-				end
-				else begin
-					sdfsd
-				end
+				if (start) next = RECEIVING;
+				else next = IDLE;
+			end
+
+			// check if start was de-asserted
+			// if so, move to DONE state
+			// otherwise, still receiving data
+
+			RECEIVING : begin
+				if (!start) next = DONE;
+				else next = RECEIVING;
+			end
+
+			// final results only last 1 cycle
+			DONE : next = IDLE;
+
 		endcase
 	end
-
-
 
 	/*************************************************************************/
 	/* FSM Block 3: assigning outputs										 */
@@ -88,18 +99,48 @@ module findMax (
 
 	always@(posedge clk or negedge reset) begin
 
+		// if reset was asserted, set outputs to zero
 		if (reset) begin
 			maxValue	<= 8'b0;
-			done		<= 1'b1;
+			done		<= 1'b0;
 		end
 
 		else begin
 
 			case(next)
-				S0, S1 :
-				S2, S3 :
 
-			endcase
-			done	<= 1'b1;
+				// check if starting to receive data
+				// if so, update maxValue - otherwise, keep at zero
+
+				IDLE : begin
+
+					if (start) maxValue <= inputA;
+					else maxValue <= 8'b0;
+
+					done <= 1'b0;
+				end
+
+				// compare current maxValue against new data input
+				// update if needed - retain previous value otherwise
+
+				RECEIVING : begin
+
+					if (inputA > maxValue) begin
+						maxValue <= inputA;
+					end
+
+					else begin
+						maxValue <= maxValue;
+					end
+
+					done <= 1'b0;
+
+				// set done flag high to indicate processing finished
+				DONE : begin
+					maxValue <= maxValue;
+					done <= 1'b1;
+				end
+
+			endcase			
 		end
 endmodule
